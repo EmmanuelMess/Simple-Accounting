5ccfeb06c7dcef17bf5d0683dffb80fad3c4ce5b
Emmanuel <emmanuelbendavid@gmail.com>
2017-01-04 18:44:28 -0300

    Simplified

--- SimpleAccounting/app/src/main/java/com/emmanuelmess/simpleaccounting/db/TableGeneral.java
+++ SimpleAccounting/app/src/main/java/com/emmanuelmess/simpleaccounting/db/TableGeneral.java
@@ -39,17 +39,8 @@ public class TableGeneral extends Database {
 		String sql;
 
 		/*I made a mistake on update 1.1.4, this should undo that*/
-		if(oldVersion == 3) {
-			sql = "CREATE TEMPORARY TABLE temp(" + COLUMNS[0] + "," + COLUMNS[1] + "," + COLUMNS[2] + "," + COLUMNS[3] + ");" +
-					"INSERT INTO temp SELECT " + COLUMNS[0] + "," + COLUMNS[1] + "," + COLUMNS[2] + "," + COLUMNS[3] + " FROM " + TABLE_NAME + ";" +
-					"DROP TABLE " + TABLE_NAME + ";" +
-					"CREATE TABLE " + TABLE_NAME + "(" + COLUMNS[0] + "," + COLUMNS[1] + "," + COLUMNS[2] + "," + COLUMNS[3] + ");" +
-					"INSERT INTO " + TABLE_NAME + " SELECT " + COLUMNS[0] + "," + COLUMNS[1] + "," + COLUMNS[2] + "," + COLUMNS[3] + " FROM temp;" +
-					"DROP TABLE temp;";
-			db.execSQL(sql);//"copy, drop table, create new table, copy back" technique bc ALTER...DROP COLUMN isn't in SQLite
-
-			oldVersion = 2;
-		}
+		if(oldVersion == 3)
+			oldVersion = 1;
 
 		switch (oldVersion) {
 			case 1:
