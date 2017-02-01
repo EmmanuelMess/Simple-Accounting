70d557e7682d3bf101607151d5fc1f32d117cfcf
Emmanuel <emmanuelbendavid@gmail.com>
2017-02-01 15:03:41 -0300

    Keyboard is working

--- SimpleAccounting/app/src/main/java/com/emmanuelmess/simpleaccounting/activities/CurrencyPicker.java
+++ SimpleAccounting/app/src/main/java/com/emmanuelmess/simpleaccounting/activities/CurrencyPicker.java
@@ -3,7 +3,6 @@ package com.emmanuelmess.simpleaccounting.activities;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.preference.DialogPreference;
 import android.support.v4.view.ViewCompat;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -13,8 +12,6 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
@@ -26,13 +23,14 @@ import com.emmanuelmess.simpleaccounting.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * @author Emmanuel
  *         on 24/1/2017, at 22:55.
  */
 
-public class CurrencyPicker extends DialogPreference {
+public class CurrencyPicker extends DialogPreferenceWithKeyboard implements View.OnClickListener {
 	private static final String KEY = "currency_picker";
 	private static final ArrayList<String> DEFAULT_VALUE = new ArrayList<>();
 
@@ -40,6 +38,10 @@ public class CurrencyPicker extends DialogPreference {
 	private ArrayList<String> currentValue = new ArrayList<>();
 	private LayoutInflater inflater;
 	private boolean firstTimeItemHeighted = true;
+	private SparseIntArray itemPos = new SparseIntArray(1);
+	private RangedStructure itemPosRanges = new RangedStructure();
+	private LinearLayout linearLayout;
+	private ScrollViewWithMaxHeight scrollView;
 
 	public CurrencyPicker(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -60,143 +62,19 @@ public class CurrencyPicker extends DialogPreference {
 		super.onPrepareDialogBuilder(builder);
 	}
 
+
 	@Override
 	public void onBindDialogView(View view) {
-		SparseIntArray itemPos = new SparseIntArray(1);
-		RangedStructure itemPosRanges = new RangedStructure();
-
-		view.findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				ScrollViewWithMaxHeight scrollView = ((ScrollViewWithMaxHeight) view.findViewById(R.id.scrollerView));
-				LinearLayout linearLayout = ((LinearLayout) view.findViewById(R.id.scrollView));
-				inflater.inflate(R.layout.currencypicker_dialog_item, linearLayout);
-
-				int childIndex = linearLayout.getChildCount() - 1;
-				View item = linearLayout.getChildAt(childIndex);
-
-				item.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-					boolean alreadyLoaded = false;
-
-					@Override
-					public void onGlobalLayout() {
-						if (!alreadyLoaded) {
-							itemPos.append(childIndex, item.getTop());
-							itemPosRanges.add(item.getTop(), item.getBottom());
-							alreadyLoaded = true;
-						}
-					}
-				});
-
-				item.getLayoutParams().width = linearLayout.getWidth();
-
-				if (linearLayout.getChildCount() == 2 && firstTimeItemHeighted) {
-					firstTimeItemHeighted = false;
-					scrollView.setMaxHeight(linearLayout.getChildAt(0).getHeight()*4);
-				}
-
-				item.findViewById(R.id.move).setOnTouchListener(new View.OnTouchListener() {
-					float dY;
-
-					private void move(int getToPos, int itemIndex) {
-						if (getToPos == itemIndex) return;
-
-						boolean direction = getToPos > itemIndex;
-						int toBeMovedIndex = itemIndex + (direction? +1:-1);
-						if (toBeMovedIndex != getToPos)
-							move(getToPos, toBeMovedIndex);
-
-						swap(itemIndex, toBeMovedIndex);
-					}
-
-					@Override
-					public boolean onTouch(View v1, MotionEvent event) {
-						switch (event.getAction()) {
-
-							case MotionEvent.ACTION_DOWN:
-								ViewCompat.animate(item).z(5).setDuration(0).start();
-								dY = ViewCompat.getY(item) - event.getRawY();
-								break;
-							case MotionEvent.ACTION_MOVE:
-								float moveTo = event.getRawY() + dY;
-
-								if (moveTo < ViewCompat.getY(linearLayout))
-									moveTo = ViewCompat.getY(linearLayout);
-								else if (moveTo > ViewCompat.getTranslationY(scrollView)
-										+ scrollView.getBottom() - item.getHeight())
-									moveTo = ViewCompat.getTranslationY(scrollView)
-											+ scrollView.getBottom() - item.getHeight();
-
-								ViewCompat.animate(item).y(moveTo).setDuration(0).start();
-
-								int overItemIndex = itemPosRanges.get((int) (ViewCompat.getY(item) + item.getHeight()/2f));
-
-								move(childIndex, overItemIndex);
-
-								//reposition
-								for (int i = 0; i < linearLayout.getChildCount(); i++) {
-									if (childIndex == i) continue;
-									ViewCompat.animate(linearLayout.getChildAt(i)).z(2.5f)
-											.y(itemPos.get(i)).z(0).setDuration(0).start();
-								}
-								break;
-							case MotionEvent.ACTION_UP:
-								moveTo = itemPos.get(itemPosRanges.get((int) (ViewCompat.getY(item) + item.getHeight()/2f)));
-								ViewCompat.animate(item).y(moveTo).z(0).setDuration(0).start();
-								break;
-							default:
-								return false;
-						}
-						return true;
-					}
-				});
-
-				EditText text = ((EditText) item.findViewById(R.id.text));
-				text.setOnFocusChangeListener((v1, hasFocus)->{
-					item.findViewById(R.id.delete).setVisibility(hasFocus? View.VISIBLE:View.INVISIBLE);
-
-					InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-					imm.showSoftInput(text, hasFocus? WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE:
-							InputMethodManager.RESULT_UNCHANGED_SHOWN);
-				});
-				text.addTextChangedListener(new Utils.SimpleTextWatcher() {
-					@Override
-					public void afterTextChanged(Editable s) {
-						if (currentValue.size() > childIndex)
-							currentValue.set(childIndex, s.toString());
-						else
-							currentValue.add(childIndex, s.toString());
-					}
-				});
-
-				item.findViewById(R.id.delete).setOnClickListener((v1)->{
-					itemPos.removeAt(itemPos.size()-1);
-					itemPosRanges.remove(itemPosRanges.size()-1);
-					linearLayout.removeView(item);
-
-					if (childIndex > 0 && linearLayout.getChildAt(childIndex - 1) != null)
-						linearLayout.getChildAt(childIndex - 1).findViewById(R.id.text).requestFocus();
-
-				});
-			}
-
-			private void swap(int i1, int i2) {
-				swapItemPos(i1, i2);
-				itemPosRanges.swap(i1, i2);
-			}
-
-			private void swapItemPos(int i1, int i2) {
-				//swap
-				Integer m = itemPos.get(i1);
-				itemPos.removeAt(i1);
-				itemPos.append(i1, itemPos.get(i2));
-				itemPos.removeAt(i2);
-				itemPos.append(i2, m);
-			}
-		});
+		linearLayout = ((LinearLayout) view.findViewById(R.id.scrollView));
+		scrollView = ((ScrollViewWithMaxHeight) view.findViewById(R.id.scrollerView));
+		view.findViewById(R.id.add).setOnClickListener(this);
 
 		super.onBindDialogView(view);
+	}
+
+	@Override
+	public boolean needInputMethod() {
+		return true;
 	}
 
 	@Override
@@ -214,8 +92,13 @@ public class CurrencyPicker extends DialogPreference {
 	@Override
 	protected void onDialogClosed(boolean positiveResult) {
 		// When the user selects "OK", persist the new value
-		if (positiveResult)
+		if (positiveResult) {
+			for (int i = 0; i < currentValue.size(); i++)
+				if (Utils.equal(currentValue.get(i).replace(" ", ""), ""))
+					currentValue.remove(i);
+
 			persistStringList(currentValue);
+		}
 	}
 
 	@Override
@@ -234,8 +117,8 @@ public class CurrencyPicker extends DialogPreference {
 	 *
 	 * @param value The value to persist.
 	 * @return True if the Preference is persistent. (This is not whether the
-	 *         value was persisted, since we may not necessarily commit if there
-	 *         will be a batch commit later.)
+	 * value was persisted, since we may not necessarily commit if there
+	 * will be a batch commit later.)
 	 * @see #persistString(String)
 	 * @see #getPersistedInt(int)
 	 */
@@ -256,10 +139,10 @@ public class CurrencyPicker extends DialogPreference {
 	 * Attempts to get a persisted ArrayList&lt;String&gt; from the {@link android.content.SharedPreferences}.
 	 *
 	 * @param defaultValue The default value to return if either this
-	 *            Preference is not persistent or this Preference is not in the
-	 *            SharedPreferences.
+	 *                     Preference is not persistent or this Preference is not in the
+	 *                     SharedPreferences.
 	 * @return The value from the SharedPreferences or the default return
-	 *         value.
+	 * value.
 	 * @see #getPersistedString(String)
 	 * @see #persistInt(int)
 	 */
@@ -272,9 +155,135 @@ public class CurrencyPicker extends DialogPreference {
 	}
 
 	private int getAbsoluteYInScreen(View v) {
-		int[] m = new int[] {0, 0};
+		int[] m = new int[]{0, 0};
 		v.getLocationOnScreen(m);
 		return m[1];
+	}
+
+	@Override
+	public void onClick(View v) {
+		inflater.inflate(R.layout.currencypicker_dialog_item, linearLayout);
+		View item = linearLayout.getChildAt(linearLayout.getChildCount() - 1);
+
+		item.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+			boolean alreadyLoaded = false;
+			int childIndex = linearLayout.getChildCount() - 1;
+
+			@Override
+			public void onGlobalLayout() {
+				if (!alreadyLoaded) {
+					itemPos.append(childIndex, item.getTop());
+					itemPosRanges.add(item.getTop(), item.getBottom());
+					alreadyLoaded = true;
+				}
+			}
+		});
+
+		item.getLayoutParams().width = linearLayout.getWidth();
+
+		if (linearLayout.getChildCount() == 2 && firstTimeItemHeighted) {
+			firstTimeItemHeighted = false;
+			scrollView.setMaxHeight(linearLayout.getChildAt(0).getHeight()*4);
+		}
+
+		currentValue.add("");
+
+		item.findViewById(R.id.move).setOnTouchListener(new View.OnTouchListener() {
+			float dY;
+
+			private void move(int getToPos, int itemIndex) {
+				if (getToPos == itemIndex) return;
+
+				boolean direction = getToPos > itemIndex;
+				int toBeMovedIndex = itemIndex + (direction? +1:-1);
+				if (toBeMovedIndex != getToPos)
+					move(getToPos, toBeMovedIndex);
+
+				swap(itemIndex, toBeMovedIndex);
+			}
+
+			@Override
+			public boolean onTouch(View v1, MotionEvent event) {
+				int childIndex = getChildIndex(item);
+
+				switch (event.getAction()) {
+					case MotionEvent.ACTION_DOWN:
+						ViewCompat.animate(item).z(5).setDuration(0).start();
+						dY = ViewCompat.getY(item) - event.getRawY();
+						break;
+					case MotionEvent.ACTION_MOVE:
+						float moveTo = event.getRawY() + dY;
+
+						if (moveTo < ViewCompat.getY(linearLayout))
+							moveTo = ViewCompat.getY(linearLayout);
+						else if (moveTo > ViewCompat.getTranslationY(scrollView)
+								+ scrollView.getBottom() - item.getHeight())
+							moveTo = ViewCompat.getTranslationY(scrollView)
+									+ scrollView.getBottom() - item.getHeight();
+
+						ViewCompat.animate(item).y(moveTo).setDuration(0).start();
+
+						int overItemIndex = itemPosRanges.get((int) (ViewCompat.getY(item) + item.getHeight()/2f));
+
+						move(childIndex, overItemIndex);
+
+						//reposition
+						for (int i = 0; i < linearLayout.getChildCount(); i++) {
+							if (childIndex == i) continue;
+							ViewCompat.animate(linearLayout.getChildAt(i)).z(2.5f)
+									.y(itemPos.get(i)).z(0).setDuration(0).start();
+						}
+						break;
+					case MotionEvent.ACTION_UP:
+						moveTo = itemPos.get(itemPosRanges.get((int) (ViewCompat.getY(item) + item.getHeight()/2f)));
+						ViewCompat.animate(item).y(moveTo).z(0).setDuration(0).start();
+						break;
+					default:
+						return false;
+				}
+				return true;
+			}
+		});
+
+		EditText text = ((EditText) item.findViewById(R.id.text));
+		text.setOnFocusChangeListener((v1, hasFocus)->{
+			item.findViewById(R.id.delete).setVisibility(hasFocus? View.VISIBLE:View.INVISIBLE);
+		});
+		text.addTextChangedListener(new Utils.SimpleTextWatcher() {
+			@Override
+			public void afterTextChanged(Editable s) {
+				currentValue.set(getChildIndex(item), s.toString());
+			}
+		});
+
+		item.findViewById(R.id.delete).setOnClickListener((v1)->{
+			int childIndex = getChildIndex(item);
+
+			currentValue.remove(childIndex);
+			itemPos.removeAt(itemPos.size() - 1);
+			itemPosRanges.remove(itemPosRanges.size() - 1);
+			linearLayout.removeView(item);
+
+			if (childIndex > 0 && linearLayout.getChildAt(childIndex - 1) != null)
+				linearLayout.getChildAt(childIndex - 1).findViewById(R.id.text).requestFocus();
+
+		});
+	}
+
+	private void swap(int i1, int i2) {
+		Collections.swap(currentValue, i1, i2);
+
+		Integer m = itemPos.get(i1);
+		itemPos.removeAt(i1);
+		itemPos.append(i1, itemPos.get(i2));
+		itemPos.removeAt(i2);
+		itemPos.append(i2, m);
+
+		itemPosRanges.swap(i1, i2);
+	}
+
+	private int getChildIndex(View v) {
+		return linearLayout.indexOfChild(v);
 	}
 
 }
