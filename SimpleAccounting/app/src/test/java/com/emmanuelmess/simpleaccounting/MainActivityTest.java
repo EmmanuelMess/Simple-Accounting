a7d2c1a75c66f5f46b6ee5de8ba58c9c1222b3fe
Emmanuel M <emmanuelbendavid@gmail.com>
2018-05-28 17:59:14 -0300

    test: [extended] FAB test, [new] simple arithmetics test

--- SimpleAccounting/app/src/test/java/com/emmanuelmess/simpleaccounting/MainActivityTest.java
+++ SimpleAccounting/app/src/test/java/com/emmanuelmess/simpleaccounting/MainActivityTest.java
@@ -4,8 +4,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
+import android.widget.EditText;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import com.emmanuelmess.simpleaccounting.activities.views.LedgerRow;
 import com.emmanuelmess.simpleaccounting.activities.views.LedgerView;
+import com.emmanuelmess.simpleaccounting.utils.SimpleBalanceFormatter;
+import com.google.common.collect.Table;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +19,8 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
+
+import java.math.BigDecimal;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -27,8 +35,40 @@ public class MainActivityTest {
                 .create().start().resume().visible().get();
         LedgerView table = activity.findViewById(R.id.table);
         FloatingActionButton fab = activity.findViewById(R.id.fab);
+        int tableChildCount = table.getChildCount();
+        assertEquals(1, tableChildCount);
+
+        for(int i = 1; i <= 30; i++) {
+            fab.callOnClick();
+            assertEquals(tableChildCount+i, table.getChildCount());
+        }
+    }
+
+    @Test
+    public void testArithmetic() {
+        setShowTutorial(false);
+        MainActivity activity =  Robolectric.buildActivity(MainActivity.class)
+                .create().start().resume().visible().get();
+        LedgerView table = activity.findViewById(R.id.table);
+        FloatingActionButton fab = activity.findViewById(R.id.fab);
         fab.callOnClick();
-        assertEquals(2, table.getChildCount());
+
+        TableRow row = (TableRow) table.getChildAt(table.getEditableRow());
+        EditText credit = row.findViewById(R.id.editCredit);
+        EditText debit = row.findViewById(R.id.editDebit);
+        TextView balance = row.findViewById(R.id.textBalance);
+        SimpleBalanceFormatter formatter = new SimpleBalanceFormatter();
+
+        BigDecimal zero = BigDecimal.ZERO.setScale(1); //0.0
+
+
+        assertEquals(formatter.format(zero), balance.getText().toString());
+
+        String c = 300 + "";
+        credit.setText(c);
+        String d = 300 + "";
+        debit.setText(d);
+        assertEquals(formatter.format(zero), balance.getText().toString());
     }
 
     private void setShowTutorial(boolean show) {
