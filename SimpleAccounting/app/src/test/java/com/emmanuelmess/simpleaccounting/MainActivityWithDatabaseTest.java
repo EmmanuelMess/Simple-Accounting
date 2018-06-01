6743a954b83fea2de450ee9dc738c74e4899090e
Emmanuel M <emmanuelbendavid@gmail.com>
2018-05-31 21:48:36 -0300

    test: [new] base for testing database

---
+++ SimpleAccounting/app/src/test/java/com/emmanuelmess/simpleaccounting/MainActivityWithDatabaseTest.java
@@ -0,0 +1,15 @@
+package com.emmanuelmess.simpleaccounting;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class MainActivityWithDatabaseTest extends MainActivityTest {
+    // TODO: 29/05/18 implement
+}
