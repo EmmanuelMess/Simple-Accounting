9a14981a678fe9ad65324e1cb82b52530f8ba1be
Emmanuel M <emmanuelbendavid@gmail.com>
2019-01-14 16:24:58 -0300

    Configured mail sender and separated strings for fdroid and play

--- SimpleAccounting/app/src/fdroid/java/com/emmanuelmess/simpleaccounting/SimpleAccountingApplication.kt
+++ SimpleAccounting/app/src/fdroid/java/com/emmanuelmess/simpleaccounting/SimpleAccountingApplication.kt
@@ -7,13 +7,14 @@ import android.widget.Toast
 import org.acra.ACRA
 import org.acra.annotation.AcraCore
 import org.acra.annotation.AcraHttpSender
+import org.acra.annotation.AcraMailSender
 import org.acra.annotation.AcraToast
 
 @AcraCore(buildConfigClass = BuildConfig::class)
-@AcraHttpSender(uri = "https://emmanuelmess.cloudant.com/acra-simpleaccounting/_design/acra-storage/_update/report",
-        httpMethod = org.acra.sender.HttpSender.Method.PUT,
-        basicAuthLogin = "greardelecarecessingstin",
-        basicAuthPassword = "607cb9c826c6f7e4eca1bee1540f8e285e5a0a17")
+@AcraMailSender(
+    mailTo = "emmanuelbendavid@gmail.com",
+    reportAsFile = true
+)
 @AcraToast(resText = R.string.crash_toast_text)
 class SimpleAccountingApplication : Application() {
     override fun attachBaseContext(base: Context) {
